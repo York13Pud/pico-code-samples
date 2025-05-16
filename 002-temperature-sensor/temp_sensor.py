c0310226d9e74c7672354141e652f2c6f661acff
Dev Neil A <16964133+York13Pud@users.noreply.github.com>
2025-05-16 21:14:40 +0100

    Added code sample for the temp sensor

---
+++ 002-temperature-sensor/temp_sensor.py
@@ -0,0 +1,50 @@
+# --- Import the required libraries / modules:
+from machine import ADC
+
+
+def main() -> None:
+    """_summary_
+    
+    This is the main function of the program. It will read the value
+    of the temperature sensor on a Pico 2 and display it in both
+    degree(s) C and F in the terminal.
+    
+    _returns_
+    
+    None: Nothing is returned
+    """
+    
+    # --- Define which ADC pin to use to connect to the temp sensor.
+    # --- This will always be pin 4 as it is onboard the Pico 1 / 2.
+    # --- You can access it using either ADC(4) or ADC(ADC.CORE_TEMP):
+    # sensor = ADC(4)
+    sensor = ADC(ADC.CORE_TEMP)
+    
+    # --- Get the digital value of an analogue reading from 
+    # --- the temp sensor as a 16-bit value:
+    adc_value = sensor.read_u16()
+    print(f"adc_value: {adc_value}")
+    
+    # --- Multiply the adc_value by the max voltage 
+    # --- divided by the max value (16-bits) to give the voltage:
+    sensor_reading_volts = adc_value * (3.3 / 65535)
+    print(f"sensor_reading: {sensor_reading_volts}")
+    
+    # --- Next, convert the reported voltage from the 
+    # --- sensor_reading_volts variable and convert it to a celsius value.
+    # --- The numbers in the formula is specific to the 
+    # --- Pico / Pico 2's temp sensor:
+    temperature = 27 - (sensor_reading_volts - 0.706) / 0.001721
+    print(f"temperature: {temperature}\n")
+    
+    # --- Display the temperature to two decimal places.
+    # --- Note: using deg. C / F as ° symbol is not supported
+    # --- on the Pico 2 due to the lack of extended ASCII
+    # --- and the unicode char is not supported for °:
+    print(f"Temp Sensor Reading: {'{:.2f}'.format(temperature)} deg. C")
+    print(f"Temp Sensor Reading: {'{:.2f}'.format(temperature * 9 / 5 + 32)} deg. F")
+
+    
+# --- Run the program:
+if __name__ == "__main__":
+    main()
